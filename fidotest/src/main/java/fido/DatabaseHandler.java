4b3af45fae47f4d4e80640b16cd1fb88b4a70b20
tikhonishchenko <89725412+tikhonishchenko@users.noreply.github.com>
2021-10-16 22:51:19 +0300

    deleted some imports

--- fidotest/src/main/java/fido/DatabaseHandler.java
+++ fidotest/src/main/java/fido/DatabaseHandler.java
@@ -1,10 +1,5 @@
 package fido;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
 import java.sql.*;
 
 public class DatabaseHandler extends JDBC{
